4f85ea3d49d4d7172a56477aafb5d333e95f09d6
henriqueferretti <henriqueferretti42@gmail.com>
2026-04-11 19:56:43 +0100

    Projeto sprint 7 - QA27

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-e19b6f4f-6cce-4971-8000-7cf46beabf1c.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
